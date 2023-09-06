e4ba05c35586426b47acafde4c8caf86db579123
Nayanaswar07 <144015276+Nayanaswar07@users.noreply.github.com>
2023-09-06 16:59:07 +0530

    Update and rename HelloWorld.java to Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,4 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
-class HelloWorld {
+class Palindrome {
     public static void main(String[] args) {
        int n = 1221;
        int i=n;
